78ec290329ff6636c733f7e95a920e58d44e7b95
RobertLangton01 <53015645+RobertLangton01@users.noreply.github.com>
2019-07-22 12:50:34 +0100

    Update webScraper.js

--- webScraper.js
+++ webScraper.js
@@ -6,6 +6,25 @@
 var myargs = process.argv.slice(2);
 let emailAddress = myargs[0];
 
+/* function validateEmail
+   This function take in a general email address
+   and checks whether this email is in a valid form,
+   i.e. in the form string @ string.string
+   This function uses the test method on the
+   regular expression /\S+@\S+\.\S/
+   to perform this.
+   Input: string
+   output: boolean
+
+   In regular expressions, \S is a non-whitespace
+   character, following something with a + indicates it
+   may be repeated more than once.
+*/
+function validateEmail(email) {
+    var emailTester = /\S+@\S+\.\S/;
+    return emailTester.test(email);
+}
+
 /* function urlFinder
    
    Accepts a string as an agrument and 
@@ -17,14 +36,10 @@ function urlFinder(email) {
     if(email == undefined) {
         throw new Error("Error: no email address entered.");
     }
+    if(validateEmail(email) == false) {
+        throw new Error("Error: Invalid email adress.");
+    }
     var ind = email.indexOf("@");
-    var dot = email.indexOf(".");
-    if(ind == -1 || dot == -1) {
-        throw new Error("Error: not a valid email address:" + email);
-    }
-    if(email.slice(dot) != ".com" && email.slice(dot) != ".co.uk") {
-        throw new Error("Error: not a valid email address:" + email);
-    }
     var end = email.slice(ind + 1);
     var url = "https://www.";
     url += end;
@@ -34,7 +49,8 @@ function urlFinder(email) {
 var request = require('request');
 var cheerio = require('cheerio');
 var Knwl = require('knwl.js');
-var knwlInstance = new Knwl('english');
+var knwlInstance1 = new Knwl('english');
+var knwlInstance2 = new Knwl('english');
 
 /* We use try in anticipation of an incorrectly entered email address.
    
@@ -48,21 +64,31 @@ var knwlInstance = new Knwl('english');
 try {
     request(urlFinder(emailAddress) , function (error, response, html) {  
         if (!error && response.statusCode == 200) {
-            // const $ = cheerio.load(html);
-            knwlInstance.register('dates', require('knwl.js/default_plugins/dates'));
-            knwlInstance.register('phones', require('knwl.js/default_plugins/phones'));
-            knwlInstance.register('emails', require('knwl.js/default_plugins/emails'));
+            var $ = cheerio.load(html);
+            var web = $.html();
+            var web1 = web.replace(/[\s]/g, '-');
+            var web2 = web1.replace(/[\", \+]/g, ' ');
 
-            knwlInstance.init(html);
-            var dates = knwlInstance.get('dates');
-            var phones = knwlInstance.get('phones');
-            var emails = knwlInstance.get('emails');
+            knwlInstance1.register('dates', require('knwl.js/default_plugins/dates'));
+            knwlInstance1.register('emails', require('knwl.js/default_plugins/emails'));
+            knwlInstance2.register('phones', require('knwl.js/default_plugins/phones'));
 
-            console.log(dates);
-            console.log(phones);
-            console.log(emails);
+            knwlInstance1.init(web);
+            knwlInstance2.init(web2);
+            var dates = knwlInstance1.get('dates');
+            var emails = knwlInstance1.get('emails');
+            var phones = knwlInstance2.get('phones');
+
+            for(let i = 0; i < phones.length; i++) {
+                console.log(phones[i]);
+            }
+
+            for(let i = 0; i < emails.length; i++) {
+                console.log(emails[i]);
+            }
         }
     });
 } catch(error) {
     console.log("Please enter a correct email address as a command argument.");
 }
+
